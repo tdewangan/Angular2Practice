a6462933dad5a783cafaf889879553997e43c86e
Tribhuwan <dewangan.tribhuwan@gmail.com>
2017-07-15 23:34:11 +0530

    reactive-forms example intergrated with heroes page

--- src/app/form-example/form-example.component.ts
+++ src/app/form-example/form-example.component.ts
@@ -1,18 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { states } from './../states';
+import { Hero } from './../hero';
+import { Address } from './../address';
 
 @Component({
   selector: 'app-form-example',
   templateUrl: './form-example.component.html',
   styleUrls: ['./form-example.component.css']
 })
-export class FormExampleComponent implements OnInit {
-  heroForm = new FormGroup({
-    name: new FormControl()
-  });
-  constructor() { }
-
-  ngOnInit() {
+export class FormExampleComponent implements OnInit, OnChanges {
+  heroForm: FormGroup;
+  states = states;
+  @Input()
+  hero: Hero;
+  constructor(private fb: FormBuilder) {
+    this.createForm();
   }
+  ngOnInit() { }
+  ngOnChanges() {
+    this.heroForm.reset({
+      name: this.hero.name,
+      address: this.hero.addresses[0] || new Address()
+    });
+  }
+  createForm() {
+    this.heroForm = this.fb.group({
+      name: ['', Validators.required],
+      address: this.fb.group({
+        street: '',
+        city: '',
+        state: '',
+        zip: '',
+      }),
+      power: '',
+      sidekick: ''
+    });
+  }
+
 
 }
